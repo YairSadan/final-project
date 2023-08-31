7686621f6e1ef26a12daa07ce4370e76a849aed8
Yair Sadan <yairsadan1@gmail.com>
2023-08-31 16:27:28 +0300

    deled

--- src/components/ChatScreen/Header/Header.jsx
+++
@@ -1,7 +0,0 @@
-const Header = ({ username}) => {
-  return (
-    <div className="shadow border-4 w-full flex place-content-center text-2xl">Chat with {username }</div>
-  )
-}
-
-export default Header
